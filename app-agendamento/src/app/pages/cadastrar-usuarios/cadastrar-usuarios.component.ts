4bfd9c366ff47755a4bb198715b4da42da37b8e5
Fabrício Oliveira <fabriciooliveira.hub@outlook.com>
2025-04-13 00:56:47 -0300

    feat: criando formulario para cadastro

--- app-agendamento/src/app/pages/cadastrar-usuarios/cadastrar-usuarios.component.ts
+++ app-agendamento/src/app/pages/cadastrar-usuarios/cadastrar-usuarios.component.ts
@@ -1,11 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastrar-usuarios',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './cadastrar-usuarios.component.html',
-  styleUrl: './cadastrar-usuarios.component.scss'
+  styleUrls: ['./cadastrar-usuarios.component.scss']
 })
 export class CadastrarUsuariosComponent {
+  formUsuario!: FormGroup;
+  mostrarSenha = false;
 
+  constructor(private fb: FormBuilder) {
+    this.formUsuario = this.fb.group({
+      nome: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      telefone: ['', [Validators.required]]
+    });
+  }
+
+  toggleSenha(): void {
+    this.mostrarSenha = !this.mostrarSenha;
+  }
+
+  cadastrarUsuario(): void {
+    if (this.formUsuario.valid) {
+      const dados = this.formUsuario.value;
+      console.log('Usuário cadastrado:', dados);
+      // Aqui você pode fazer um POST para uma API, por exemplo
+    } else {
+      this.formUsuario.markAllAsTouched();
+    }
+  }
 }
