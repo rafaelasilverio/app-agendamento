a9b1b16e611d57852d079475df99d5c9d0b82b70
Fabrício Oliveira <fabriciooliveira.hub@outlook.com>
2025-05-06 21:00:04 -0300

    feat: criando componente sidebar menu usuario

---
+++ app-agendamento/src/app/layout/sidebar/sidebar.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [CommonModule],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss'
+})
+export class SidebarComponent {
+  constructor(private router: Router) { }
+
+  paginaMeusAgendamentos() {
+    this.router.navigate(['/my-schedules']);
+  }
+}
--- app-agendamento/src/app/pages/tela-usuario/tela-usuario.component.ts
+++ app-agendamento/src/app/pages/tela-usuario/tela-usuario.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../layout/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-tela-usuario',
-  imports: [],
+  imports: [CommonModule, SidebarComponent],
   templateUrl: './tela-usuario.component.html',
   styleUrl: './tela-usuario.component.scss'
 })
