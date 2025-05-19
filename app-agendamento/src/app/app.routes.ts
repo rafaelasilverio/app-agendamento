1a06caa3ebb1b97617b2bc64bdd0f3a83c65ad80
Fabrício Oliveira <fabriciooliveira.hub@outlook.com>
2025-05-18 21:15:07 -0300

    refactor: adicionando novas rotas para o componente cliente e tipo perfil

--- app-agendamento/src/app/app.routes.ts
+++ app-agendamento/src/app/app.routes.ts
@@ -4,18 +4,29 @@ export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
   { path: 'login', loadComponent: () => import('./pages/tela-login/tela-login.component').then(m => m.TelaLoginComponent) },
+  {
+    path: 'register-choice',
+    loadComponent: () => import('./pages/tipo-conta-selector/tipo-conta-selector.component')
+      .then(m => m.TipoContaSelectorComponent)
+  },
   { path: 'register', loadComponent: () => import('./pages/tela-cadastro/tela-cadastro.component').then(m => m.TelaCadastroComponent) },
-  { path: 'user', loadComponent: () => import('./pages/tela-usuario/tela-usuario.component').then(m => m.TelaUsuarioComponent) },
-  { path: 'profile-settings', loadComponent: () => import('./pages/tela-usuario/components/tela-configuracoes-perfil/tela-configuracoes-perfil.component').then(m => m.TelaConfiguracoesPerfilComponent) },
-  { path: 'my-schedules', loadComponent: () => import('./pages/tela-usuario/components/tela-meus-agendamentos/tela-meus-agendamentos.component').then(m => m.TelaMeusAgendamentosComponent) },
   { path: 'service-catalog', loadComponent: () => import('./pages/catalogo-servicos/catalogo-servicos.component').then(m => m.CatalogoServicosComponent) },
   { path: 'team', loadComponent: () => import('./pages/tela-equipe/tela-equipe.component').then(m => m.TelaEquipeComponent) },
   { path: 'about', loadComponent: () => import('./pages/sobre/sobre.component').then(m => m.SobreComponent) },
   { path: 'recover-password', loadComponent: () => import('./pages/tela-recuperacao-senha/tela-recuperacao-senha.component').then(m => m.TelaRecuperacaoSenhaComponent) },
   { path: 'faq', loadComponent: () => import('./pages/tela-faq/tela-faq.component').then(m => m.TelaFaqComponent) },
-  { path: 'register-service', loadComponent: () => import('./pages/tela-usuario/components/tela-cadastrar-servico/tela-cadastrar-servico.component').then(m => m.TelaCadastrarServicoComponent) },
-  { path: 'manage-services', loadComponent: () => import('./pages/tela-usuario/components/tela-gerenciamento-servicos/tela-gerenciamento-servicos.component').then(m => m.TelaGerenciamentoServicosComponent) },
-  { path: 'edit-services/:id', loadComponent: () => import('./pages/tela-usuario/components/tela-editar-servico/tela-editar-servico.component').then(m => m.TelaEditarServicoComponent) },
+
+  // Layout com sidebar para usuários autenticados
+  {
+    path: '',
+    loadComponent: () => import('./layout/usuario-layout/usuario-layout.component').then(m => m.UsuarioLayoutComponent),
+    children: [
+      { path: 'user', loadComponent: () => import('./pages/tela-usuario/tela-usuario.component').then(m => m.TelaUsuarioComponent) },
+      { path: 'profile-settings', loadComponent: () => import('./pages/tela-usuario/components/tela-configuracoes-perfil/tela-configuracoes-perfil.component').then(m => m.TelaConfiguracoesPerfilComponent) },
+      { path: 'my-schedules', loadComponent: () => import('./pages/tela-usuario/components/tela-meus-agendamentos/tela-meus-agendamentos.component').then(m => m.TelaMeusAgendamentosComponent) },
+      { path: 'manage-services', loadComponent: () => import('./pages/tela-usuario/components/tela-gerenciamento-servicos/tela-gerenciamento-servicos.component').then(m => m.TelaGerenciamentoServicosComponent) },
+    ]
+  },
 
   { path: '**', redirectTo: '/home' }
 ];
