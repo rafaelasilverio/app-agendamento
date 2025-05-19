e2c4269dd66997b501f818ed16148776be22ab9c
Fabrício Oliveira <fabriciooliveira.hub@outlook.com>
2025-05-18 21:03:22 -0300

    feat: criando tela para o usuario selecionar o tipo de cadastro

---
+++ app-agendamento/src/app/pages/tipo-conta-selector/tipo-conta-selector.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-tipo-conta-selector',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tipo-conta-selector.component.html',
+  styleUrl: './tipo-conta-selector.component.scss'
+})
+export class TipoContaSelectorComponent {
+
+  constructor(private router: Router) { }
+
+  escolherTipo(tipo: 'CLIENT' | 'PROVIDER') {
+    this.router.navigate(['/login'], { queryParams: { tipo } });
+  }
+}
