7fbe4bc2481a1f6ac11b85f321f91fbb5e609dd2
Fabrício Oliveira <fabriciooliveira.hub@outlook.com>
2025-05-25 17:49:06 -0300

    refactor: adicionando descricoes

--- app-agendamento/src/app/pages/home/home.component.ts
+++ app-agendamento/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselImgService } from './service/carousel-img.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -13,9 +14,16 @@ export class HomeComponent implements OnInit {
 
   services: any[] = [];
 
-  constructor(private carouselImgService: CarouselImgService) { }
+  constructor(
+    private carouselImgService: CarouselImgService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.services = this.carouselImgService.getCarouselItems();
   }
+
+  irParaEscolhaPerfil() {
+  this.router.navigate(['/register-choice']);
+}
 }
